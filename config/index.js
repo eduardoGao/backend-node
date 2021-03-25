0e1b9e87ccc215e5306a24156670cf2e38bf860d
Eduardo Diaz <lalo.diaz1896@gmail.com>
2021-03-24 23:45:41 -0600

    :memo: [VIEW]

--- config/index.js
+++ config/index.js
@@ -1,5 +1,15 @@
+require('dotenv').config()
+
 const config = {
-  port: 3000
+  //CONFIG
+  port: 3000,
+  //cors= process.env.CORS,
+
+  //MONGO DB
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME
 }
 
 module.exports = { config }
--- routes/apiMovies.js
+++ routes/apiMovies.js
@@ -1,20 +1,79 @@
 const express = require('express')
 
-const dataMovies = require('../utils/mockData/dataMovies')
-
+const MoviesService = require('../services/movies')
 
 function apiMovies(app) {
   const router = express.Router()
   
   app.use('/api/movies', router)
 
+  const moviesService = new MoviesService()
+
   router.get('/', async (req, res, next) => {
     try {
-      const movies = await Promise.resolve(dataMovies)
+      const movies = await moviesService.getMovies()
 
       res.status(200).json({
         movies,
         message: 'Movies listed'
+      })
+    } catch (error) {
+      next(error)
+    }
+  })
+
+  router.get('/:movieId', async (req, res, next) => {
+    const id = req.query
+    try {
+      const movie = await moviesService.getMovie(id)
+
+      res.status(200).json({
+        Movie: {
+          movie
+        },
+        message: 'Movie'
+      })
+    } catch (error) {
+      next(error)
+    }
+  })
+
+  router.post('/', async (req, res, next) => {
+    //const {body: movie} = req
+
+    try {
+      const movie = await moviesService.createMovie()
+
+      res.status(200).json({
+        Created: movie,
+        message: 'Movie created'
+      })
+    } catch (error) {
+      next(error)
+    }
+  })
+
+  router.put('/:movieId', async (req, res, next) => {
+    try {
+      const movie = await moviesService.updateMovie()
+
+      res.status(200).json({
+        Update: movie,
+        message: 'Movies updated'
+      })
+    } catch (error) {
+      next(error)
+    }
+  })
+
+  router.delete('/:movieId', async (req, res, next) => {
+    const movieId = req.params
+    try {
+      const movie = await moviesService.deleteMovie({ movieId })
+
+      res.status(200).json({
+        deleted: movie,
+        message: 'Movie deleted'
       })
     } catch (error) {
       next(error)
